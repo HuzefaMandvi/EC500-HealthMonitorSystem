cc1303087525f6fa77a9c16ffa344d58a457c9a1
Ayush Upneja <ayushupneja@dhcp-wifi-8021x-155-41-57-52.bu.edu>
2019-02-06 16:53:59 -0500

    made data file into a function

--- data.py
+++ data.py
@@ -2,18 +2,16 @@ import random
 #a heart rate, b bpm1, c bpm2, d bo
 
 
-a = random.randint(60, 100)
-b = random.randint(120, 140)
-c = random.randint(80, 90)
-d = random.randint(75, 100)
+def data_pull():
+        a = random.randint(60, 100)
+        b = random.randint(120, 140)
+        c = random.randint(80, 90)
+        d = random.randint(75, 100)
 
-pdata = {
-        "heart_rate": a,
-        "blood_pressure1": b,
-        "blood_pressure2": c,
-        "blood_oxygen": d
-}
-print pdata.get("heart_rate")
-print pdata.get("blood_pressure1")
-print pdata.get("blood_pressure2")
-print pdata.get("blood_oxygen")
+        pdata = {
+                "heart_rate": a,
+                "blood_pressure1": b,
+                "blood_pressure2": c,
+                "blood_oxygen": d
+        }
+        return pdata
