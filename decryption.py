bfeef742473618a92eb3fcc7c249c4de91d772de
thaorell <khanhchuong7396@gmail.com>
2019-02-06 17:37:00 -0500

    added the decryption module

---
+++ decryption.py
@@ -0,0 +1,11 @@
+import Crypto
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+import base64
+import ast
+
+def decrypt(encrypted, private_key):
+    cipher = PKCS1_OAEP.new(private_key)
+    decrypted_msg = cipher.decrypt(encrypted)
+    print(decrypted_msg)
+    return decrypted_msg
