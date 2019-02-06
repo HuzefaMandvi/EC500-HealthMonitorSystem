4915ff5ffb42e0a26d5e71e0f2844f5f245662cb
Ayush Upneja <ayushupneja@dhcp-wifi-8021x-155-41-57-52.bu.edu>
2019-02-06 17:00:37 -0500

    fixed variable names

--- data.py
+++ data.py
@@ -1,17 +1,15 @@
 import random
-#a heart rate, b bpm1, c bpm2, d bo
-
 
 def data_pull():
-        a = random.randint(60, 100)
-        b = random.randint(120, 140)
-        c = random.randint(80, 90)
-        d = random.randint(75, 100)
+        hr = random.randint(60, 100)
+        bp1 = random.randint(120, 140)
+        bp2 = random.randint(80, 90)
+        bo = random.randint(75, 100)
 
         pdata = {
-                "heart_rate": a,
-                "blood_pressure1": b,
-                "blood_pressure2": c,
-                "blood_oxygen": d
+                "heart_rate": hr,
+                "blood_pressure1": bp1,
+                "blood_pressure2": bp2,
+                "blood_oxygen": bo,
         }
         return pdata
