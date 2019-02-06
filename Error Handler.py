774cf6add0284b305447f8fc2025e49566489e54
Tariq Ahsan <tahsan@bu.edu>
2019-02-06 17:24:12 -0500

    made the error handler use data:

--- Error Handler.py
+++ Error Handler.py
@@ -1,5 +1,10 @@
 
-def Error_Handler(bp1_reading, bp2_reading, bo_reading, hr_reading):
+def Error_Handler(data):
+    hr_reading = data.get("heart_rate")
+    bp1_reading = data.get(“blood_pressure1”)
+    bp2_reading = data.get(“blood_pressure2”)
+    bo_reading = data.get(“blood_oxygen”)
+
     alert = false
 
     if bp1_reading > 140 or bp1_reading < 120 or bp2_reading > 90 or bp2_reading < 80:
