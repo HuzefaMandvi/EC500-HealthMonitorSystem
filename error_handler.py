4fed23c33d9a7b18abdedfbaa818951a16c17586
Huzefa Mandviwala <hzmandviwala@gmail.com>
2019-02-09 15:42:49 -0500

    true -> True, false -> False in error handler

--- error_handler.py
+++ error_handler.py
@@ -1,19 +1,19 @@
 
 def Error_Handler(data):
     hr_reading = data.get("heart_rate")
-    bp1_reading = data.get(“blood_pressure1”)
-    bp2_reading = data.get(“blood_pressure2”)
-    bo_reading = data.get(“blood_oxygen”)
+    bp1_reading = data.get("blood_pressure1")
+    bp2_reading = data.get("blood_pressure2")
+    bo_reading = data.get("blood_oxygen")
 
-    alert = false
+    alert = False
 
     if bp1_reading > 140 or bp1_reading < 120 or bp2_reading > 90 or bp2_reading < 80:
-        alert = true
+        alert = True
 
     if hr_reading > 120 or hr_reading < 50:
-        alert = true
+        alert = True
 
     if bo_reading > 110 or bo_reading < 70:
-        alert = true
+        alert = True
 
     return alert
