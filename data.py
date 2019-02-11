93fe91a092566ece46bf4296fb7124893675c8a0
maxCut <maxCut@github.com>
2019-02-11 17:39:40 -0500

    updated data.py for synchronys requirement

--- data.py
+++ data.py
@@ -1,10 +1,29 @@
 import random
+import threading
+import time
 
-def data_pull():
+hr = 0
+bp1 = 0
+bp2 = 0
+bo = 0
+
+def init():
         hr = random.randint(60, 100)
         bp1 = random.randint(120, 140)
         bp2 = random.randint(80, 90)
         bo = random.randint(75, 100)
+        t = threading.Thread(target=callback)
+        t.start()
+
+def callback():
+        hr = random.randint(60, 100)
+        bp1 = random.randint(120, 140)
+        bp2 = random.randint(80, 90)
+        bo = random.randint(75, 100)
+        time.sleep(random.randint(1,10))
+        callback()
+
+def data_pull():
 
         pdata = {
                 "heart_rate": hr,
